ac07357f224aac45e072bc0b3c992b8720d92da1
Nabb <ca202329593@alm.buap.mx>
2025-02-13 00:22:05 -0600

    Update ProyectoFinalBeta.c

--- Proyecto 1/ProyectoFinalBeta.c
+++ Proyecto 1/ProyectoFinalBeta.c
@@ -2,104 +2,157 @@
 #include <string.h>
 #include <stdlib.h>
 
-typedef struct Cliente{
+//estrcuturas, recursos, personas, historial, seguimiento
+typedef struct Persona{
     char turno[10];
     char Nombre[50];
     char Nacionalidad[50];
     char Motivo[100];
     int Urgencia;
-}Cliente;
+}Persona;
 
 typedef struct Nodo{
-    Cliente cliente;
+    Persona persona;
     struct Nodo* siguiente;
 }Nodo;
 
-Nodo* crearNodo(Cliente cliente) {
+typedef struct Recurso{
+    char nombre[50];
+    int cantidad;
+    struct Recurso* siguiente;
+}Recurso;
+
+typedef struct Historial{
+    Persona persona;
+    char recurso[50];
+    struct Historial* anterior; 
+    struct Historial* siguiente;
+}Historial;
+
+typedef struct Seguimiento{
+    Persona persona;
+    struct Seguimiento* siguiente;  
+}Seguimiento;
+
+//Crear un nodo
+
+Nodo* crearNodo(Persona persona){
     Nodo* nuevoNodo = (Nodo*)malloc(sizeof(Nodo));
-    if (nuevoNodo == NULL) {
+    if (nuevoNodo == NULL){
         printf("Error, no se puede asignar memoria");
         return NULL;
     }
-    strcpy(nuevoNodo->cliente.turno, cliente.turno);    
-    strcpy(nuevoNodo->cliente.Nombre, cliente.Nombre);   
-    strcpy(nuevoNodo->cliente.Nacionalidad, cliente.Nacionalidad);  
-    strcpy(nuevoNodo->cliente.Motivo, cliente.Motivo);    
-    nuevoNodo->cliente.Urgencia = cliente.Urgencia;       
+    strcpy(nuevoNodo->persona.turno, persona.turno);
+    strcpy(nuevoNodo->persona.Nombre, persona.Nombre); 
+    strcpy(nuevoNodo->persona.Nacionalidad, persona.Nacionalidad);
+    strcpy(nuevoNodo->persona.Motivo, persona.Motivo);
+    nuevoNodo->persona.Urgencia = persona.Urgencia; 
     nuevoNodo->siguiente = NULL;
     return nuevoNodo;
 }
 
+Nodo* seleccionarPersona(Nodo* lista) {
+    if (lista == NULL) {
+        printf("No hay personas en la lista.\n");
+        return NULL;
+    }
 
-void insertarPorUrgencia(Nodo** lista, Nodo* nuevoNodo) {
+    printf("\nSeleccione una persona de la lista:\n");
+    int i = 1;
+    Nodo* temp = lista;
+    
+    while (temp != NULL) {
+        printf("%d. %s (Turno: %s, Urgencia: %d)\n", i, temp->persona.Nombre, temp->persona.turno, temp->persona.Urgencia);
+        temp = temp->siguiente;
+        i++;
+    }
+
+    int opcion;
+    printf("Ingrese el numero correspondiente a la persona:\n");
+    scanf("%d", &opcion);
+    
+    temp = lista;
+    for (int j = 1; j < opcion && temp != NULL; j++) {
+        temp = temp->siguiente;
+    }
+
+    if (temp == NULL) {
+        printf("Opción inválida.\n");
+        return NULL;
+    }
+
+    return temp;
+}
+
+void insertarUrgencia(Nodo** lista, Nodo* nuevoNodo){
     Nodo* actual = *lista;
     Nodo* anterior = NULL;
 
-    while (actual != NULL && actual->cliente.Urgencia <= nuevoNodo->cliente.Urgencia) {
+    while (actual != NULL && actual->persona.Urgencia <= nuevoNodo->persona.Urgencia){
         anterior = actual;
         actual = actual->siguiente;
     }
 
-    if (anterior == NULL) {
-        nuevoNodo->siguiente = *lista;
+    if (anterior == NULL){
+        nuevoNodo->siguiente == *lista;
         *lista = nuevoNodo;
     } else {
-        nuevoNodo->siguiente = actual;
+        nuevoNodo -> siguiente = actual;
         anterior->siguiente = nuevoNodo;
     }
 }
 
-void generarTurno(char turno[], int* contadorTurnos) {
-    sprintf(turno, "T%04d", (*contadorTurnos)++);
+void generarTurno(char turno[], int* contadorTurnos){
+    sprintf(turno, "%04d", (*contadorTurnos)++);
 }
 
-void datosCliente(Cliente cliente){
+void datosCliente(Persona persona){
 
     printf("-------------------\n");
     printf("Datos del nuevo cliente:\n");
-    printf("Turno: %s\n", cliente.turno);
-    printf("Nombre: %s\n", cliente.Nombre);
-    printf("Nacionalidad: %s\n", cliente.Nacionalidad);
-    printf("Motivo: %s\n", cliente.Motivo);
-    printf("Urgencia: %d\n", cliente.Urgencia);
+    printf("Turno: %s\n", persona.turno);
+    printf("Nombre: %s\n", persona.Nombre);
+    printf("Nacionalidad: %s\n", persona.Nacionalidad);
+    printf("Motivo: %s\n", persona.Motivo);
+    printf("Urgencia: %d\n", persona.Urgencia);
     printf("-------------------\n");
     
 }
 
-void operacionNuevoCliente(int *contadorTurnos, Nodo** listaClientes){
-    Cliente nuevoCliente;
-    generarTurno(nuevoCliente.turno, contadorTurnos);
+void operacionNuevaPersona(int* contadorTurnos, Nodo** listaPersonas){
+    Persona nuevaPersona;
+    generarTurno(nuevaPersona.turno, contadorTurnos);
 
     printf("\nIngrese su nombre:");
-    scanf(" %[^\n]", nuevoCliente.Nombre);
+    scanf(" %[^\n]", nuevaPersona.Nombre);
 
     printf("\nIngrese su nacionalidad:");
-    scanf(" %[^\n]", nuevoCliente.Nacionalidad);
+    scanf(" %[^\n]", nuevaPersona.Nacionalidad);
 
     printf("\nIngrese su motivo de desplazamiento:");
-    scanf(" %[^\n]", nuevoCliente.Motivo);
+    scanf(" %[^\n]", nuevaPersona.Motivo);
 
     do{
   
         printf("\nIngrese el grado de urgencia:\n1. Urgente.\n2. Regular.");
-        scanf("%d", &nuevoCliente.Urgencia);
+        scanf("%d", &nuevaPersona.Urgencia);
         
-        if (nuevoCliente.Urgencia < 1 || nuevoCliente.Urgencia > 2){
+        if (nuevaPersona.Urgencia < 1 || nuevaPersona.Urgencia > 2){
             printf("\n- - - - - - - - - - - - - - - - - - - - - - - -");
             printf("\nNumero de urgencia inexistente. Intente de nuevo");
             printf("\n- - - - - - - - - - - - - - - - - - - - - - - -\n");
 
         }
-    }while (nuevoCliente.Urgencia < 1 || nuevoCliente.Urgencia > 2);
+    }while (nuevaPersona.Urgencia < 1 || nuevaPersona.Urgencia > 2);
 
-    Nodo* nuevoNodo = crearNodo(nuevoCliente);
+    Nodo* nuevoNodo = crearNodo(nuevaPersona);
     if(nuevoNodo == NULL){
         printf("Error, no se puede crear el nodo");
         return;
     }
 
-    insertarPorUrgencia(listaClientes, crearNodo(nuevoCliente));
-    datosCliente(nuevoCliente);
+    insertarUrgencia(listaPersonas, crearNodo(nuevaPersona));
+    datosCliente(nuevaPersona);
 }
 
 void liberarLista(Nodo* lista) {
@@ -113,28 +166,204 @@ void liberarLista(Nodo* lista) {
 
 void mostrarFila(Nodo* cola) {
     printf("- - - - - - - - - - - - - - - - - - -\n");
-    printf("Clientes en la cola de prioridades:\n");
+    printf("Persona en la cola de prioridades:\n");
     printf("- - - - - - - - - - - - - - - - - - -\n");
     Nodo* temp = cola;
     if (cola == NULL)
     {
-        printf("\t* No hay clientes en la fila *");
+        printf("\t* No hay personas en la fila *");
     }
     
     while (temp != NULL) {
+        printf("--------------------------------------------\n");
         printf("Identificador: %s - Urgencia: %d - Nombre: %s - Nacionalidad: %s - Motivo de desplazamiento: %s\n",
-               temp->cliente.turno, temp->cliente.Urgencia, temp->cliente.Nombre, temp->cliente.Nacionalidad, temp->cliente.Motivo);
+               temp->persona.turno, temp->persona.Urgencia, temp->persona.Nombre, temp->persona.Nacionalidad, temp->persona.Motivo);
         temp = temp->siguiente;
     }
 }
 
-void menuCiclado(int *contadorTurnos, Nodo **listaClientes){
+void agregarRecurso(Recurso** lista, char nombre[], int cantidad){
+    Recurso* nuevoRecurso = (Recurso*)malloc(sizeof(Recurso));
+    strcpy(nuevoRecurso->nombre, nombre);
+    nuevoRecurso->cantidad = cantidad;
+    nuevoRecurso ->siguiente = *lista;
+    *lista = nuevoRecurso;
+}
+
+void mostrarRecursos(Recurso* lista){
+    printf("\n-Recursos disponibles-\n");
+    Recurso* temp = lista;
+    while (temp != NULL){
+        printf("- %s: %d disponibles\n", temp->nombre, temp->cantidad);
+        temp = temp->siguiente;
+    }
+}
+
+void asignarRecurso(Nodo** listaPersonas, Recurso** listaRecursos, Historial** historial) {
+    if (*listaPersonas == NULL) {
+        printf("No hay clientes para asignar recursos.\n");
+        return;
+    }
+
+    // Seleccionar a la persona
+    Nodo* personaSeleccionada = seleccionarPersona(*listaPersonas);
+    if (personaSeleccionada == NULL) return;
+
+    // Mostrar recursos
+    if (*listaRecursos == NULL) {
+        printf("No hay recursos disponibles.\n");
+        return;
+    }
+
+    mostrarRecursos(*listaRecursos);
+
+    // Seleccionar recurso
+    printf("\nIngrese el nombre del recurso que desea asignar: ");
+    char nombreRecurso[50];
+    scanf(" %[^\n]", nombreRecurso);
+
+    Recurso* temp = *listaRecursos;
+    while (temp != NULL) {
+        if (strcmp(temp->nombre, nombreRecurso) == 0 && temp->cantidad > 0) {
+            temp->cantidad--;
+            printf("Recurso asignado a %s: %s\n", personaSeleccionada->persona.Nombre, temp->nombre);
+
+            // Guardar en historial
+            Historial* nuevoHistorial = (Historial*)malloc(sizeof(Historial));
+            nuevoHistorial->persona = personaSeleccionada->persona;
+            strcpy(nuevoHistorial->recurso, temp->nombre);
+            nuevoHistorial->anterior = NULL;
+            nuevoHistorial->siguiente = *historial;
+
+            if (*historial != NULL) {
+                (*historial)->anterior = nuevoHistorial;
+            }
+
+            *historial = nuevoHistorial;
+            return;
+        }
+        temp = temp->siguiente;
+    }
+
+    printf("Recurso no encontrado o sin disponibilidad.\n");
+}
+
+
+void mostrarHistorial(Historial* historial){
+    printf("Historial de asignaciones:\n");
+    Historial* temp = historial;
+    while (temp != NULL){
+        printf("Cliente: %s - Recurso; %s \n", temp->persona.Nombre, temp->recurso);
+        temp = temp->siguiente;
+    }
+}
+
+void agregarSeguimiento(Seguimiento** cabeza, Persona persona){
+    Seguimiento* nuevoSeguimiento = (Seguimiento*)malloc(sizeof(Seguimiento));
+    nuevoSeguimiento->persona = persona;
+
+    if(*cabeza == NULL){
+        nuevoSeguimiento->siguiente = nuevoSeguimiento;
+        *cabeza = nuevoSeguimiento;
+    } else {
+        Seguimiento* temp = *cabeza;
+        while (temp->siguiente != *cabeza){
+            temp = temp -> siguiente;
+        }
+        temp->siguiente = nuevoSeguimiento;
+        nuevoSeguimiento->siguiente = *cabeza;
+    }
+}
+
+void mostrarSeguimiento(Seguimiento* cabeza){
+    if(cabeza == NULL){
+        printf("No hay casos de seguimiento");
+        return;
+    }
+
+    Seguimiento* temp = cabeza;
+    printf("--Casos de seguimiento--\n");
+    do{
+        printf("Identificador: %s - Nombre: %s\n", temp->persona.turno, temp->persona.Nombre);
+        temp = temp->siguiente;    
+    }while(temp != cabeza);
+}
+
+void generarReporte(Nodo* listaClientes, Recurso* listaRecurso, Historial* historial, Seguimiento* seguimiento){
+    printf("\n--- Reporte semanal ---\n");
+
+    //Atendidos
+    printf("-Personas Atendidas-\n");
+    Nodo* temp = listaClientes;
+    while(temp != NULL){
+        printf("- %s (Turno: %s)\n", temp->persona.Nombre, temp->persona.turno);
+        temp = temp->siguiente;
+    }
+
+    //Recursos
+    printf("\n-Recursos Disponibles-\n");
+    Recurso* tempRecursos = listaRecurso;
+    while (tempRecursos != NULL){
+        printf("- %s\n", tempRecursos->nombre);
+        tempRecursos = tempRecursos->siguiente;
+    }
+
+    //Historial
+    printf("-Historial de asignaciones-\n");
+    Historial* tempHistorial = historial;
+    while(tempHistorial != NULL){
+        printf("- %s: %s\n", tempHistorial->persona.Nombre, tempHistorial->recurso);
+        tempHistorial = tempHistorial->siguiente;
+    }
+
+    //Seguimiento
+    printf("-Historial de asignaciones-\n");
+    if(seguimiento == NULL){
+        printf("No hay casos en seguimiento");
+    } else{
+        Seguimiento* tempSeguimiento = seguimiento;
+        do{
+            printf("- %s (turno:%s)\n", tempSeguimiento->persona.Nombre, tempSeguimiento->persona.turno);
+            tempSeguimiento = tempSeguimiento->siguiente;
+        }while(tempSeguimiento != seguimiento);
+    }
+}
+
+void liberarRecursos(Recurso* lista) {
+    Recurso* temp;
+    while (lista != NULL) {
+        temp = lista;
+        lista = lista->siguiente;
+        free(temp);
+    }
+}
+
+void liberarHistorial(Historial* lista) {
+    Historial* temp;
+    while (lista != NULL) {
+        temp = lista;
+        lista = lista->siguiente;
+        free(temp);
+    }
+}
+
+void liberarSeguimiento(Seguimiento* lista) {
+    if (lista == NULL);
+        Seguimiento* temp = lista;
+        do{
+            Seguimiento* siguiente = temp->siguiente;
+            free(temp);
+            temp = siguiente;
+        }while (temp != lista);
+}
+
+void menuCiclado(int* contadorTurnos, Nodo** listaPersonas, Recurso** listaRecursos, Historial** historial, Seguimiento** seguimiento){
     int opcion;
 
     do {
         printf("\n........................\n");
         printf("\n\tMENU\nOpciones disponibles:\n");
-        printf("1. Nuevo cliente\n2. Visualizar Lista\n3. Salir\n");
+        printf("1. Nuevo cliente\n2. Visualizar Lista\n3. Asignar Recurso\n4. Ver Historial\n5. Generar Reporte\n6. Salir\n");
         printf("\n........................\n");
         printf("\nIngrese el numero de opcion deseado: ");
         scanf("%d", &opcion);
@@ -144,27 +373,47 @@ void menuCiclado(int *contadorTurnos, Nodo **listaClientes){
         switch (opcion) {
             case 1:
                 //Crear Nuevo Cliente
-                operacionNuevoCliente(contadorTurnos, listaClientes);
+                operacionNuevaPersona(contadorTurnos, listaPersonas);
                 break;
             case 2:
                 // Ver lista 
-                    mostrarFila(*listaClientes);                
+                mostrarFila(*listaPersonas);                
                 break;
             case 3:
+                asignarRecurso(listaPersonas, listaRecursos, historial);
+                break;
+            case 4:
+                mostrarHistorial(*historial);
+                break;
+            case 5:
+                generarReporte(*listaPersonas, *listaRecursos, *historial, *seguimiento);
+                break;
+            case 6:
                 break;
             default:
                 printf("Opcion no valida");
                 break;
         }
-    } while (opcion != 3);
+    } while (opcion != 6);
 }
 
-int main() {
-    Nodo* listaClientes = NULL;
+int main(){
+    Nodo* listaPersonas = NULL;
+    Recurso* listaRecursos = NULL;
+    Historial* historial = NULL;
+    Seguimiento* seguimiento = NULL;
     int contadorTurnos = 0;
 
-    menuCiclado(&contadorTurnos, &listaClientes);
+    agregarRecurso(&listaRecursos, "Alimentos", 10);
+    agregarRecurso(&listaRecursos, "Refugio", 5);
+    agregarRecurso(&listaRecursos, "Asesoria Legal", 15);
 
-    liberarLista(listaClientes); 
+    menuCiclado(&contadorTurnos, &listaPersonas, &listaRecursos, &historial, &seguimiento);
+
+    liberarLista(listaPersonas);
+    liberarRecursos(listaRecursos);
+    liberarHistorial(historial);
+    liberarSeguimiento(seguimiento);
+
     return 0;
 }
